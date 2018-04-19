4820351ed02ea7daa593d6c0f2de2ee5dbc4b288
Katherine Kato <katherine.a.kato@gmail.com>
2018-04-18 19:31:28 -0700

    Added files

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Katherine Kato',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const PortfolioPage = () => (
+  <div>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis adipisci, voluptatum, nisi velit praesentium error quia explicabo voluptas ad recusandae enim. Recusandae sequi non vel voluptas. Labore omnis provident ex, perferendis esse, voluptate atque alias officiis dolor eligendi itaque qui?</p>
+    <p>Now go build something great.</p>
+  </div>
+)
+
+export default PortfolioPage
