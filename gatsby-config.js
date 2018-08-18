95770062019716e11ba0e052cdd167cbbf3fb7ec
Katherine Kato <katherine.a.kato@gmail.com>
2018-08-18 16:01:20 -0700

    v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
