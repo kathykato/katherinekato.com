4728e670512a3f3c7a91ba012d135c92aa93f3cf
Katherine Kato <katherine.a.kato@gmail.com>
2019-07-05 21:00:03 -0700

    Edited files

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
-import { Link } from "gatsby"
 import React from "react"
+
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
 const Header = () => (
   <header className="intro">
@@ -8,7 +9,7 @@ const Header = () => (
       <div className="column-xs-12 column-sm-12 column-md-10">
         <h1>Hi! I'm Katherine Kato, a front-end web developer based in Seattle.</h1>
         <p className="lead">I enjoy crafting digital experiences through design and code. I specialize in UI/UX design and front-end web development, focusing on turning ideas into delightful and meaningful solutions.</p>
-        <p className="lead"><Link className="link" href="#about">More about me</Link></p>
+        <p className="lead"><AnchorLink className="link" href="#about">More about me</AnchorLink></p>
         </div>
       </div>
     </div>
